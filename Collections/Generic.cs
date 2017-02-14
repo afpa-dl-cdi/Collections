55847f2fa8d085bd42056009cd6cbd504cf2a38c
david di Marcantonio <david@code4mac.com>
2017-02-14 11:13:37 +0100

    aspect générique et liste simple

---
+++ Collections/Generic.cs
@@ -0,0 +1,62 @@
+﻿namespace Collections
+{
+    class Generic <T>
+    {
+        private int _index = 0;
+        const int TAILLEPARDEFAUT = 100;
+        private T[] _data; 
+
+        /// <summary>
+        /// Constructeur général qui prend la taille par défaut
+        /// </summary>
+        public Generic()
+        {
+            _data = new T[TAILLEPARDEFAUT]; // use 'T' as the data type
+        }
+
+        /// <summary>
+        ///  Constructeur permettant de redéfinir la taille du tableau 
+        /// </summary>
+        /// <param name="taille">Taille du tableau</param>
+        public Generic( int taille)
+        {
+            _data = new T[taille];
+        }
+
+        /// <summary>
+        /// Ajoute un élément
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            _data[_index++] = item;
+        }
+
+        /// <summary>
+        /// Récupère un élément
+        /// </summary>
+        /// <returns></returns>
+        public T Get(int position)
+        {
+            return _data[position];
+        }
+
+        /// <summary>
+        /// Afficheur de tableau
+        /// </summary>
+        /// <typeparam name="Ti"></typeparam>
+        /// <param name="tableau"></param>
+        public static void afficherTableau <Ti> (Ti [] tableau)
+        {
+            foreach (Ti item in tableau)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+
+        public static void afficher <Th>( Th [] tabl)
+        {
+
+        }
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -10,6 +10,47 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            // ********* UTLISATION D UNE CLASSE GENERIQUE **************** 
+            Generic <int> genericInt = new Generic<int>();
+            genericInt.Add(5);
+            genericInt.Add(3);
+            Console.WriteLine(genericInt.Get(0));
+
+            Generic<string> genericString = new Generic<string>();
+            genericString.Add("Un mot");
+            Console.WriteLine(genericString.Get(0));
+
+            // ************** UTILISATION D UNE METHODE GENERIQUE **********
+            string[] data = { "un", "deux", "trois" };
+            Generic<string>.afficherTableau<string>(data);  // dans notre cas la classe est aussi générique mais aurait pu ne pas l'être. Nous aurions appelé Generic.afficherTableau<string>(data);
+
+
+            // ***************** LISTES ************
+            List<int> uneListe = new List<int>();
+            uneListe.Add(0);
+            uneListe.Add(1);
+            uneListe.Add(6);
+            uneListe.Reverse();
+            uneListe.Insert(1, 8);
+            Console.WriteLine(uneListe.Average());
+
+
+            // *************** FIFO / file ***************
+            Queue<string> imprimante = new Queue<string>();
+
+            // *************** LIFO / Pile **************
+            Stack<int> pile = new Stack<int>();
+
+            // *************** DICTIONNAIRE *************
+
+
+
+            // **************** LISTE CHAINEE ************
+
+
+
+            
+               
         }
     }
 }
